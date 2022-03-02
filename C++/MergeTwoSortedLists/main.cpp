da8a4426f7b654f670d78f331695ec60c53bfb39
Gideon <niemelaig123@gmail.com>
2022-03-02 08:27:53 -0400

    Removed some redundant code

--- C++/MergeTwoSortedLists/main.cpp
+++ C++/MergeTwoSortedLists/main.cpp
@@ -17,5 +17,24 @@ int main() {
 
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+     if (list1 == nullptr && list2 == nullptr) {
+            return nullptr;
+        }
+        if (list1 == nullptr) {
+            return list2;
+        }
+        if (list2 == nullptr) {
+            return list1;
+        }
 
+        // Compare values in each list
+        if (list1->val < list2->val) {
+            // Set the head to list that is greater
+            list1->next = mergeTwoLists(list1->next, list2);
+
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
 }
